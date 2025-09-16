5bb8f7ff6a1b7282004df9bd92d20f5d5ca62e74
benjalegree <149977722+benjalegree@users.noreply.github.com>
2025-09-16 19:34:08 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+
+// Deja pasar todo. (Luego, si querés subdominios, lo afinamos)
 export function middleware(_req: NextRequest) {
-  return NextResponse.next(); // no reescribe nada
+  return NextResponse.next();
 }
