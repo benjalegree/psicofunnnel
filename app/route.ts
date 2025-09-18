83211af848ec8c5b6413dc4cba3602e82f9aaa1a
benjalegree <149977722+benjalegree@users.noreply.github.com>
2025-09-18 13:41:24 +0200

    Delete app/route.ts

--- app/route.ts
+++
@@ -1,7 +0,0 @@
-// app/route.ts
-import { NextResponse } from 'next/server';
-
-export async function GET(req: Request) {
-  const to = new URL('/index.html', req.url);
-  return NextResponse.redirect(to, 308);
-}
