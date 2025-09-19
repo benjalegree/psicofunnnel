22af2bad9d597ca22390464d69d73324036c22ef
benjalegree <149977722+benjalegree@users.noreply.github.com>
2025-09-19 18:58:17 +0200

    Delete app directory

--- app/crm/page.tsx
+++
@@ -1,7 +0,0 @@
-// Redirige /crm a tu HTML estático del CRM
-import { redirect } from 'next/navigation';
-export const dynamic = 'force-static';
-
-export default function CRM() {
-  redirect('/crm/index.html');
-}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-// Redirige la home a tu HTML estático
-import { redirect } from 'next/navigation';
-export const dynamic = 'force-static';
-
-export default function Home() {
-  redirect('/index.html');
-}
